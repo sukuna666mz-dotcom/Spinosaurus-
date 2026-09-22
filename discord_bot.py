8f88751c81b93d0c88434996c0519d88b78073b5
MR_MZ <sukuna666mz@gmail.com>
2026-09-22 22:33:27 +0300

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -41,7 +41,7 @@ YTDL_OPTIONS = {
     "no_warnings": True,
     "default_search": "ytsearch",
     "source_address": "0.0.0.0",
-     "extractor_args": {"youtube": {"player_client": ["android", "web"]}},
+    "extractor_args": {"youtube": {"player_client": ["android", "web"]}},
     "cookiefile": "cookies.txt",
     "format": "bestaudio/best"
 }
